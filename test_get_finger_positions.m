0959bec87b01795fe43515d4cbb18fc3dae4e6e2
Hidalgo Carvajal <ge97vos@mytum.de>
2020-11-30 17:54:32 +0100

    Latest update on 30.11.2020

--- test_get_finger_positions.m
+++ test_get_finger_positions.m
@@ -1,5 +1,8 @@
 %% Definition of fingers/palm/arm segments
 
+st1.subjects{1}.build_results();
+st1.subjects{2}.build_results();
+
 thumb_finger = ["RH1", "RTH1", "RTH2", "RTH3"];
 index_finger = ["RH2", "RIF1", "RIF2", "RIF3"];
 third_finger = ["RH3", "RTF1", "RTF2", "RTF3"];
@@ -9,5 +12,10 @@ palm = ["RH1", "RH2", "RH3", "RH4", "RH5", "RH6"];
 arm = ["RFA1", "RFA2", "RWRA", "RWRB", "RH1", "RH6"];
 seg = ["RH1", "RH2", "RH3"];
 
-[traj, ang] = track_seg(st1, 2, ring_finger, 'ring');
-[traj_sn, ang_sn] = snap_seg(traj, ang, 100);
+n_smp = 750;
+[traj, ang] = track_seg(st1, 2, index_finger, 'index');
+[traj_sn, ang_sn] = snap_seg(traj, ang, n_smp);
+T = [st1.subjects{2}.ax(4:6, n_smp), st1.subjects{2}.ax(7:9, n_smp), st1.subjects{2}.ax(10:12, n_smp), st1.subjects{2}.ax(1:3, n_smp)];
+T = [T; 0 0 0 1];
+T*[1;0;0;1]
+
